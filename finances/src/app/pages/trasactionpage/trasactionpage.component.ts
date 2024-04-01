a0f863c67e95e0ebb82cb3f68cfdb9ff16470964
bervalim <bernardogvalim@gmail.com>
2024-04-01 17:41:09 -0300

    feat/getter-transacition-list

--- finances/src/app/pages/trasactionpage/trasactionpage.component.ts
+++ finances/src/app/pages/trasactionpage/trasactionpage.component.ts
@@ -2,12 +2,20 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TransactionFormComponent } from '../../components/forms/transaction-form/transaction-form.component';
 import { TransactionSumComponent } from '../../components/transaction-sum/transaction-sum.component';
+import { TransactionService } from '../../../services/transaction.service';
 
 @Component({
   selector: 'app-trasactionpage',
   standalone: true,
   imports: [CommonModule, TransactionFormComponent, TransactionSumComponent],
+  providers: [TransactionService],
   templateUrl: './trasactionpage.component.html',
   styleUrl: './trasactionpage.component.scss',
 })
-export class TrasactionpageComponent {}
+export class TrasactionpageComponent {
+  constructor(private transactionService: TransactionService) {}
+
+  get transactionList() {
+    return this.transactionService.getTransactionList();
+  }
+}
